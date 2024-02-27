e3d87d0e35f668b0bdacb4ce89be82d9e79b9fcd
Joel <124399316+joelortiiz@users.noreply.github.com>
2024-02-27 11:29:55 +0100

    final commit

--- solución/src/assets/services/getEpisodes.js
+++
@@ -1,7 +0,0 @@
-
-const getEpisodes = async() => {
-  const episodes = await fetch("https://rickandmortyapi.com/api/episode")
-  return await episodes.json();
-}
-
-export default getEpisodes
--- solución/src/assets/services/getOneEpisode.js
+++
@@ -1,7 +0,0 @@
-const getOneEpisode = async (episodeId) => {
-  const result = await fetch("https://rickandmortyapi.com/api/episode/" + episodeId);
-  const episode = await result.json();
-  return episode;
-};
-
-export default getOneEpisode;
